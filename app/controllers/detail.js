be520c7166b59aa0cc09329fec64fe88c73ad5b3
Camilo Pérez <camilo.perezvr@gmail.com>
2017-03-19 15:48:12 -0500

    Implementar delete y put desde controller

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -1,23 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    actions: {
-    ajax: Ember.inject.service(),
-    savePost: function (currentPost) {
-      put('ajax').request('https://jsonplaceholder.typicode.com/posts/' + currentPost.id,{ data: currentPost })
-        .then(function success(data){
-          console.log('there the data is',data)
-        },function error(err){
-          console.log('the promise is oops',err);
-        });
-      console.log('currentPost',currentPost);
+  ajax: Ember.inject.service(),
+  actions: {
+    refreshPost: function (id) {
+      var thiz = this;
+      this.get('ajax').request('https://jsonplaceholder.typicode.com/posts/' + id).then(function success(data) {
+        thiz.set('model', data);
+      }, function error(err) {
+        console.log('erro', err)
+      });
     },
-    deletePost(id){
-      this.del('ajax').request('https://jsonplaceholder.typicode.com/posts/' + id)
-        .then(function success(data){
-          console.log('there the data is',data)
-        },function error(err){
-          console.log('the promise is oops',err);
+    savePost: function (currentPost) {
+      this.get('ajax').request('https://jsonplaceholder.typicode.com/posts/' + currentPost.id, {
+        method: 'PUT',
+        data: currentPost
+      }).then(function success(data) {
+          console.log('there the data is', data)
+        }, function error(err) {
+          console.log('the promise is oops', err);
+        });
+      console.log('currentPost', currentPost);
+    },
+    deletePost(id) {
+      this.get('ajax').request('https://jsonplaceholder.typicode.com/posts/' + id,{
+        method: 'DELETE'
+      }).then(function success(data) {
+          console.log('there the data is', data)
+        }, function error(err) {
+          console.log('the promise is oops', err);
         });
     }
   }
