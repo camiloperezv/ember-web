11e6192a291a56e592478d8831e88b0ebc0806b0
Camilo Pérez <camilo.perezvr@gmail.com>
2017-03-19 16:37:25 -0500

    Editar índex, quitar ruta, modificar lista y detalle

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
+var mainUrl = 'https://jsonplaceholder.typicode.com'
 
 export default Ember.Controller.extend({
   ajax: Ember.inject.service(),
   actions: {
     refreshPost: function (id) {
       var thiz = this;
-      
-      this.get('ajax').request('https://jsonplaceholder.typicode.com/posts/' + id).then(function success(data) {
+      this.get('ajax').request(mainUrl+'/posts/' + id).then(function success(data) {
         thiz.set('model', data);
       }, function error(err) {
         console.log('error updating', err)
@@ -14,26 +14,23 @@ export default Ember.Controller.extend({
     },
     savePost: function (currentPost) {
       var thiz = this;
-      this.get('ajax').request('https://jsonplaceholder.typicode.com/posts/' + currentPost.id, {
+      this.get('ajax').request(mainUrl+'/posts/' + currentPost.id, {
         method: 'PUT',
         data: currentPost
       }).then(function success(data) {
           alert('Action completed')
           thiz.transitionToRoute('list');
-          console.log('there the data is', data)
         }, function error(err) {
           console.log('the promise is oops', err);
         });
-      console.log('currentPost', currentPost);
     },
     deletePost(id) {
       var thiz = this;
-      this.get('ajax').request('https://jsonplaceholder.typicode.com/posts/' + id,{
+      this.get('ajax').request(mainUrl+'/posts/' + id,{
         method: 'DELETE'
       }).then(function success(data) {
           alert('Action completed')
           thiz.transitionToRoute('list');
-          console.log('there the data is', data)
         }, function error(err) {
           console.log('the promise is oops', err);
         });
--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('list');
-  this.route('info');
   this.route('detail',{path:'detail/:postId'});
 });
 
